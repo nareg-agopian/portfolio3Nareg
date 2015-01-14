a0b01a26ace1ec0d898514a781050264ae4d09c5
nareg-agopian <nareg19@outlook.com>
2015-01-14 15:05:55 -0800

    no message

--- public_html/script.js
+++ public_html/script.js
@@ -1,7 +1,56 @@
+$('document').ready(function() {
+    $('.yeah').css('background-color', 'yellow');
 
+    // $('.2').css({visibility': 'hidden', 'position': 'absolute'}):
 
-$('docuemnt').ready(function(){
+    // $('div:last p:nth-child(3)').css('backround-color', 'pink');
+
+    //$("h1").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
+
+    //$('h1').bind('click', mouseClick);
+    $('#replaceWText').css('color', 'blue');
+
+    $('#replaceWText').bind('click', replaceWText);
+
+    $('#randPara').bind('click', addAPara);
+
+    $('#removePara').bind('click', removeAPara);
     
-    $('.what').css('background-color', 'red');
+     $('#show').css('visibility', 'hidden');
+      
+     $('#hide').bind('click', hideThePage);
+     
+     $('#show').bind('click', showThePage);
     
 });
+
+function removeAPara() {
+    $('#randPara) p:last').remove();
+}
+
+function addAPara() {
+    $('#randPara').append('<p>ADDED</p>');
+}
+
+function replaceWText() {
+    $('#randPara').html('<h1>Dany loves Twilight</h1>');
+}
+      //$('#replaceWText').css('color', 'blue');
+
+
+//     $('#replaceWText').bind('click', replaceWText);
+//
+//     $('#randPara').bind('click', addPara);
+//
+//     $('#removePara').bind('click', removePara);
+     
+     function hideThePage(){
+     $('#show').css('visibility', 'visible');
+     $('div').hide('slide', {}, 2500);
+     $('#show').show('fold', {}, 2500);
+     }
+     
+     function showThePage(){
+         $('div').show ('fold', {}, 2500);
+         $('#show').hide('puff', {}, 2500);
+     }
