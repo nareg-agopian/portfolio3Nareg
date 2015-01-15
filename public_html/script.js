e6b69f1e90cb6ae3f9e71f3b7888cccf136d4a4a
nareg-agopian <nareg19@outlook.com>
2015-01-15 15:03:22 -0800

    no message

--- public_html/script.js
+++ public_html/script.js
@@ -1,14 +1,21 @@
 $('document').ready(function() {
-    $('.yeah').css('background-color', 'yellow');
+    $('.yeah').css('background-color', 'blue');
+    
+    $('div:last p:nth-child(1)').css('background-color', 'red');
+    $('div:last p:nth-child(2)').css('background-color', 'orange');
+    $('div:last p:nth-child(3)').css('background-color', 'red');
+    $('div:last p:nth-child(4)').css('background-color', 'orange');
+    $('div:last p:nth-child(5)').css('background-color', 'red');
+    $('div:last p:nth-child(6)').css('background-color', 'orange');
 
     // $('.2').css({visibility': 'hidden', 'position': 'absolute'}):
 
     // $('div:last p:nth-child(3)').css('backround-color', 'pink');
 
     //$("h1").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
-
+  
     //$('h1').bind('click', mouseClick);
-    $('#replaceWText').css('color', 'blue');
+    $('#replaceWText').css('color', 'green');
 
     $('#replaceWText').bind('click', replaceWText);
 
@@ -33,9 +40,9 @@ function addAPara() {
 }
 
 function replaceWText() {
-    $('#randPara').html('<h1>Dany loves Twilight</h1>');
+    $('#randPara').html('<h1>SURE YES!</h1>');
 }
-      //$('#replaceWText').css('color', 'blue');
+      //$('#replaceWText').css('color', 'green');
 
 
 //     $('#replaceWText').bind('click', replaceWText);
@@ -44,13 +51,13 @@ function replaceWText() {
 //
 //     $('#removePara').bind('click', removePara);
      
-     function hideThePage(){
-     $('#show').css('visibility', 'visible');
-     $('div').hide('slide', {}, 2500);
-     $('#show').show('fold', {}, 2500);
-     }
-     
-     function showThePage(){
-         $('div').show ('fold', {}, 2500);
-         $('#show').hide('puff', {}, 2500);
-     }
+function hideThePage() {
+    $('#show').css('visibility', 'visible');
+    $('div').hide('slide', {}, 2500);
+    $('#show').show('fold', {}, 2500);
+}
+
+function showThePage() {
+    $('div').show('fold', {}, 2500);
+    $('#show').hide('puff', {}, 2500);
+}
